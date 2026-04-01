f8390e6cbf846e939537247fea6c289b8cd4e356
Nishant Kumar <31658606+NishantKumar1@users.noreply.github.com>
2026-04-01 10:39:32 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,21 @@
+# First steps towards Programming
+#Fibonacci series.
+# the sum of two elements defines the next. Starting with 0 and 1, the first 10 elements will be:
+
+a,b = 0,1
+while a < 10:
+    print(a)
+    a,b = b, a+b
+
+i = 256*256
+print('The value of i is', i)
+
+a,b = 0,1
+while a<1000:
+    print(a)
+    a,b = b, a+b
+
+
+
+
+    
