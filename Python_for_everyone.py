56225c1919c96f5a6e8c36cb6d86a4dcdd1161b0
Nishant Kumar <31658606+NishantKumar1@users.noreply.github.com>
2026-04-01 10:40:36 +0530

    Add files via upload

---
+++ Python_for_everyone.py
@@ -0,0 +1,57 @@
+# Elements of Python
+x = 1
+print(x)
+
+x = x +1
+print(x)
+
+# Reserved words in Python, we can't use them else where in our program. For example, we can't use "if" as a variable name because it's a reserved word in Python.
+# The following code will give an error because "if" is a reserved word in Python:  
+# if = 5
+# print(if)
+
+# Sentences or Lines
+x = 2 # Assignment statement
+x = x + 1 #Assignment with  Expression statement
+print(x) # print Expression statement.
+
+# x is variable
+# = is assignment operator
+# 2 is a literal or constant
+# print is a function
+
+# Sequential Steps
+x = 2
+print(x)
+x = x + 2
+print(x)
+
+# When a program is runing, it flows from one step to the next.
+#  As programmers, we set up "paths" for the proram to follow.
+
+# Conditional Steps
+x = 5
+if x < 10:
+    print("Smaller")
+if x > 20:
+    print("Bigger")
+
+print("Finish")
+
+
+# Repeated Steps
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print("Blastoff!")
+
+# Loops (repeated steps) have iteration variables that change each time through a loop
+# if condition is run to check if the condition is true or false
+# while condition is run when we need to run the condition until its true.
+# For loop condition is to run when you know how many time you want to run the loop
+
+x = 15
+x = x + 5
+print(x) 
+
